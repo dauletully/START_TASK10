3a7dc95b264ec4e144298e47d744274fa1f56f87
Nurkybakyt <begaly05.05@mail.ru>
2023-10-08 15:33:27 +0600

    deleted all comments

--- Task10/ViewController.swift
+++ Task10/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  Task10
-//
-//  Created by Dinmukhammed Begaly on 08.10.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     func chageColor(){
         print("Background color chaged")
